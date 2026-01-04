04bd06f166ab9e8a0917017aaa9a61a80e9415d2
TongLongDev <tonglong.dev@gmail.com>
2026-01-04 20:08:37 +0700

    Feed + Post + ImageEditor

---
+++ src/components/Feed.tsx
@@ -0,0 +1,14 @@
+import Post from "./Post";
+
+const Feed = () => {
+  return (
+    <div className="">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Feed;
