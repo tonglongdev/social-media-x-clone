2d52f4db964c97247a8d3787ca5d5efed3149d1c
Tong Long Dev <tonglong.dev@gmail.com>
2026-01-12 11:23:53 +0700

    cleark auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,26 @@
-import LeftBar from "@/components/LeftBar";
-import RightBar from "@/components/RightBar";
 import "./globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
+// import QueryProvider from "@/providers/QueryProvider";
 
 export const metadata: Metadata = {
   title: "X Clone",
   description: "Next.js social media application project",
 };
 
-export default function RootLayout({
+export default function AppLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-8">
-            <LeftBar />
-          </div>
-          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
-            {children}
-            {modal}
-          </div>
-          <div className="hidden lg:flex ml-4 md:ml-8 flex-1">
-            <RightBar />
-          </div>
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      {/* <QueryProvider> */}
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+      {/* </QueryProvider> */}
+    </ClerkProvider>
   );
 }
